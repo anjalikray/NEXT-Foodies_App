f420629f7e6b222d6da4c4dd0bb694491535d0c6
Anjali <anjalikumari2907@gmail.com>
2024-08-28 23:12:53 +0530

    meals reder on ui

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,26 @@
-export default function MealsPage() {
-     return(
-          <>
-               <h1>Meals Page</h1>
-          </>
-     )
+import Link from 'next/link';
+
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
+export default function MealPage() {
+  return (
+     <>
+          <header className={classes.header}>
+               <h1>
+                    Delicious meals, created <span className={classes.highlight}>by you</span>
+               </h1>
+               <p>Choose your favorite recipes and cook it by yourself. It is easy and fun! </p>
+               <p className={classes.cta}>
+                    <Link href="/meals/share">
+                         Share your Favorite Recipe.  
+                    </Link>
+               </p>
+          </header>
+
+          <main className={classes.main}>
+               <MealsGrid meals={[]}/>
+          </main>
+     </>
+  );
 }
