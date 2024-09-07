4e16f8412a6114b693d608ac5c9927aef1776950
Anjali <anjalikumari2907@gmail.com>
2024-09-07 21:20:06 +0530

    state management

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,16 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
-export default async function MealPage() {
-
+async function Meals() {
      const meals = await getMeals()
+     return <MealsGrid meals={meals}/>
+}
+
+export default function MealPage() {
 
   return (
      <>
@@ -22,8 +26,11 @@ export default async function MealPage() {
                </p>
           </header>
 
+
           <main className={classes.main}>
-               <MealsGrid meals={meals}/>
+               <Suspense>
+                    <Meals />
+               </Suspense>
           </main>
      </>
   );
